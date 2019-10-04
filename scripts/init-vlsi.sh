2975384277c1a7d786eda0355b47c59eb4a43b0c
Alex Carsello <alex.carsello@gmail.com>
2019-10-04 12:03:50 -0700

    Skip submodules we don't have access to in init-vlsi

--- scripts/init-vlsi.sh
+++ scripts/init-vlsi.sh
@@ -6,8 +6,8 @@ set -o pipefail
 # Initialize HAMMER and CAD-plugins
 git submodule update --init --recursive vlsi/hammer
 git submodule update --init --recursive vlsi/hammer-cadence-plugins
-git submodule update --init --recursive vlsi/hammer-synopsys-plugins
-git submodule update --init --recursive vlsi/hammer-mentor-plugins
+#git submodule update --init --recursive vlsi/hammer-synopsys-plugins
+#git submodule update --init --recursive vlsi/hammer-mentor-plugins
 
 # Initialize HAMMER tech plugin
 if [[ $1 != *asap7* ]]; then
